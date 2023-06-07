fdb89e113a66dbc35c301994816c1904c9ca110d
adrab <adam.rabcewicz@ewolwe.pl>
2023-06-07 22:11:43 +0200

    Add .gitignore Update dotfiles/.config/home-manager/flake.nix Update dotfiles/.config/home-manager/home.nix

--- dotfiles/dot_config/home-manager/home.nix
+++ dotfiles/dot_config/home-manager/home.nix
@@ -1,177 +1,71 @@
+{ config, pkgs, ... }:
+
 {
-  config,
-  pkgs,
-  ...
-}: {
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "adrab";
+  home.homeDirectory = "/home/adrab";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    hello
+    neovim
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/adrab/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  home = {
-    username = "adrab";
-    homeDirectory = "/home/adrab";
-    stateVersion = "22.11";
-
-    packages = with pkgs; [
-      bat
-      nnn
-    ];
-
-  };
-
-
-  targets.genericLinux.enable = true;
-  fonts.fontconfig.enable = true;
-
-  #xdg.enable = true;
-  #xdg.mime.enable = true;
-
-  nixpkgs.config = {
-    allowUnfree = true;
-    allowUnfreePredicate = _: true;
-  };
-
-  programs.neovim = {
-    enable = true;
-    vimAlias = true;
-    extraConfig = ''
-      colorscheme gruvbox
-    '';
-    plugins = with pkgs.vimPlugins; [
-      gruvbox
-      vim-nix
-      {
-        plugin = kommentary;
-        type   = "lua";
-        config = ''
-          vim.api.nvim_set_keymap("n", "<C-_>", "<Plug>kommentary_line_default", {})
-          vim.api.nvim_set_keymap("v", "<C-_>", "<Plug>kommentary_visual_default", {}) 
-        '';
-      }
-      {
-        plugin = vim-visual-multi;
-        config = ''
-          let g:VM_maps = {}
-          let g:VM_maps['Find Under']         = '<M-n>'
-          let g:VM_maps['Find Subword Under'] = '<M-n>'
-          let g:VM_maps['Select All']         = '<M-C-n>'
-          let g:VM_maps['Start Regex Search'] = '<M-/>'
-          let g:VM_maps["Select Cursor Down"] = '<M-C-j>'
-          let g:VM_maps["Select Cursor Up"]   = '<M-C-k>'
-          let g:VM_maps["Skip Region"]        = '<M-x>'
-        '';
-      }
-    ];
-  };
-
-
-  programs.zsh = {
-    enable = true;
-    autocd = true;
-    #dotDir = "${config.xdg.configHome}/zsh";
-    dotDir = ".config/zsh";
-    enableAutosuggestions = true;
-    enableCompletion = true;
-    shellAliases = {
-      ll = "ls -al";
-      hm = "home-manager";
-      hmd = "cd ~/.config/home-manager";
-      hms = "home-manager switch --flake ~/.config/home-manager#adrab";
-      hme = "home-manager edit";
-      hmp = "home-manager packages";
-      hmu = "nix flake update ~/.config/home-manager && hms";
-    };
-    sessionVariables = rec {
-      EDITOR = "vim";
-      MANPAGER="nvim +Man!";
-    };
-    history = {
-      expireDuplicatesFirst = true;
-      save = 100000000;
-      size = 1000000000;
-      path = "${config.xdg.dataHome}/zsh/history";
-    };
-
-    initExtra = ''
-      autopair-init
-    '';
-
-    plugins = with pkgs; [
-
-      {
-        name = "zsh-autopair";
-        src = fetchFromGitHub {
-          owner = "hlissner";
-          repo = "zsh-autopair";
-          rev = "34a8bca0c18fcf3ab1561caef9790abffc1d3d49";
-          sha256 = "1h0vm2dgrmb8i2pvsgis3lshc5b0ad846836m62y8h3rdb3zmpy1";
-        };
-      #  file = "autopair.zsh";
-      }
-    {
-      name = "zsh-vi-mode";
-      src = builtins.fetchGit {
-        url = "https://github.com/jeffreytse/zsh-vi-mode";
-        rev = "0b1381bd2b23b966881e2bca426216a47f7f3403";
-      };
-    }
-
-    ];
-
-  };
-
-  programs.starship.enable = true;
-  programs.starship.settings = {
-    add_newline = false;
-    format = "$shlvl$username$hostname$nix_shell$git_branch$git_commit$git_state$git_status$directory$jobs$cmd_duration$character";
-    shlvl = {
-      disabled = false;
-      symbol = "ﰬ";
-      style = "bright-red bold";
-    };
-    username = {
-      style_user = "bright-white bold";
-      style_root = "bright-red bold";
-    };
-    hostname = {
-      style = "bright-green bold";
-      ssh_only = true;
-    };
-    nix_shell = {
-      symbol = "";
-      format = "[$symbol$name]($style) ";
-      style = "bright-purple bold";
-    };
-    git_branch = {
-      only_attached = true;
-      format = "[$symbol$branch]($style) ";
-      symbol = "שׂ";
-      style = "bright-yellow bold";
-    };
-    git_commit = {
-      only_detached = true;
-      format = "[ﰖ$hash]($style) ";
-      style = "bright-yellow bold";
-    };
-    git_state = {
-      style = "bright-purple bold";
-    };
-    git_status = {
-      style = "bright-green bold";
-    };
-    directory = {
-      read_only = " ";
-      truncation_length = 0;
-    };
-    cmd_duration = {
-      format = "[$duration]($style) ";
-      style = "bright-blue";
-    };
-    jobs = {
-      style = "bright-green bold";
-    };
-    character = {
-      success_symbol = "[\\$](bright-green bold)";
-      error_symbol = "[\\$](bright-red bold)";
-    };
-  };
-
 }
