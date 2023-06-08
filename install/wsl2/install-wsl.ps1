dc62d63510860c3791b271f3ef26ba465d0a0a0a
adrab <adam.rabcewicz@ewolwe.pl>
2023-06-08 17:10:44 +0200

    install-wsl.ps1 script

---
+++ install/wsl2/install-wsl.ps1
@@ -0,0 +1,16 @@
+param ($distro)
+if ($distro -eq $null) {
+$distro = read-host -Prompt "Enter a distribution name"
+}
+
+Write-Host "Installing and initializing wsl2 distribution $distro"
+wsl --install $distro --no-launch
+
+Write-Host "--- init1.sh ---"
+wsl -d $distro --cd ~ -e setup/install/wsl2/init1.sh
+
+Write-Host "--- init2.sh ---"
+wsl -d $distro --cd ~ -e setup/install/wsl2/init2.sh
+
+Write-Host "--- init3.sh ---"
+wsl -d $distro --cd ~ -e setup/install/wsl2/init3.sh
