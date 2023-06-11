46703e87201621e2a7f56d02b666ef5744883758
adrab <adam.rabcewicz@ewolwe.pl>
2023-06-11 09:08:36 +0200

    Update dotfiles/.config/powershell/profile.ps1

--- dotfiles/dot_config/powershell/profile.ps1
+++ dotfiles/dot_config/powershell/profile.ps1
@@ -1,4 +1,5 @@
 Set-Alias -Name vim -Value nvim
+Set-Alias -Name chezmoi -Value "chezmoi edit -c ~\chezmoi\chezmoi.toml"
 Set-Alias -Name che -Value "chezmoi edit"
 Set-Alias -Name chep -Value "chezmoi edit ~/.config/powershell/profile.ps1"
 
