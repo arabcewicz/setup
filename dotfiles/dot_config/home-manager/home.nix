0e3cb6769b82661000fad5d370c5b1e4102121ea
adrab <adam.rabcewicz@ewolwe.pl>
2023-07-01 12:21:24 +0200

    hm: coursier

--- dotfiles/dot_config/home-manager/home.nix
+++ dotfiles/dot_config/home-manager/home.nix
@@ -36,6 +36,9 @@
     cmatrix
     gdu
     vifm-full
+    xdragon
+    #
+    coursier
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
