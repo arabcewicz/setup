e10b37aa9482fd05c925260df160bcb2f24e7acb
adrab <adam.rabcewicz@ewolwe.pl>
2023-08-19 16:04:23 +0200

    wezterm: updated config

--- dotfiles/dot_config/powershell/profile.ps1
+++ dotfiles/dot_config/powershell/profile.ps1
@@ -1,3 +1,14 @@
+$prompt = ""
+function Invoke-Starship-PreCommand {
+    $current_location = $executionContext.SessionState.Path.CurrentLocation
+    if ($current_location.Provider.Name -eq "FileSystem") {
+        $ansi_escape = [char]27
+        $provider_path = $current_location.ProviderPath -replace "\\", "/"
+        $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
+    }
+    $host.ui.Write($prompt)
+}
+
 Set-Alias -Name vim -Value nvim
 #Set-Alias -Name chep -Value "chezmoi edit ~/.config/powershell/profile.ps1"
 
