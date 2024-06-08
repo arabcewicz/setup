f6122dc37ce5777a22bee19f3923b077fa9a47cf
adrab <adam.rabcewicz@ewolwe.pl>
2024-06-08 20:37:20 +0200

    wezterm: inactive pane

---
+++ dotfiles/dot_config/wezterm/wezterm.lua
@@ -0,0 +1,100 @@
+local wezterm = require "wezterm"
+
+local mux = wezterm.mux
+local act = wezterm.action
+local wsl_domains = wezterm.default_wsl_domains()
+
+for _, domain in ipairs(wsl_domains) do
+  domain.default_cwd = "~"
+end
+
+
+return {
+  -- default_prog = { 'pwsh.exe' },
+  color_scheme = 'Catppuccin Mocha',
+  font = wezterm.font('JetBrainsMono NF'),
+  font_size = 9.9,
+  line_height = 1.05,
+  enable_tab_bar = true,
+  use_ime = true,
+  -- treat_left_ctrlalt_as_altgr = true,
+  --[[ window_background_opacity = 0.9,
+  win32_system_backdrop = 'Acrylic', ]]
+  enable_csi_u_key_encoding = true,
+  allow_win32_input_mode = false,
+  -- debug_key_events = true,
+  term = 'xterm-256color',
+  -- term = 'wezterm',
+  inactive_pane_hsb = {
+    brightness = 0.85,
+  },
+  use_dead_keys = false,
+  scrollback_lines = 10000,
+  show_update_window = true,
+  unicode_version = 14,
+  adjust_window_size_when_changing_font_size = false,
+  -- force_reverse_video_cursor = true,
+  force_reverse_video_cursor = false,
+  window_close_confirmation = 'NeverPrompt',
+  --  window_background_opacity = 0.9,
+  --  text_background_opacity = 0.9,
+  window_frame = {
+    font = wezterm.font { family = 'Noto Sans', weight = 'Regular' },
+    font_size = 9,
+    active_titlebar_bg = '#282828',
+    inactive_titlebar_bg = '#928374',
+  },
+  colors = {
+    tab_bar = {
+      inactive_tab_edge = '#fabd2f',
+    },
+  },
+  window_padding = {
+    left = 8,
+    right = 8,
+    bottom = 0,
+    top = 0,
+  },
+  disable_default_key_bindings = true,
+  audible_bell = 'Disabled',
+  -- leader = { key = ' ', mods = 'ALT|WIN', timeout_milliseconds = 2000 },
+  keys = {
+    { key = 'Space', mods = 'ALT|WIN',       action = act.ActivateCommandPalette },
+    { key = 'h',     mods = 'ALT|WIN',       action = act.ActivatePaneDirection 'Left', },
+    { key = 'l',     mods = 'ALT|WIN',       action = act.ActivatePaneDirection 'Right' },
+    { key = 'j',     mods = 'ALT|WIN',       action = act.ActivatePaneDirection 'Down' },
+    { key = 'k',     mods = 'ALT|WIN',       action = act.ActivatePaneDirection 'Up' },
+    { key = 'h',     mods = 'ALT|WIN|SHIFT', action = act.SplitPane { direction = 'Left' } },
+    { key = 'l',     mods = 'ALT|WIN|SHIFT', action = act.SplitPane { direction = 'Right' } },
+    { key = 'j',     mods = 'ALT|WIN|SHIFT', action = act.SplitPane { direction = 'Down' } },
+    { key = 'k',     mods = 'ALT|WIN|SHIFT', action = act.SplitPane { direction = 'Up' } },
+    { key = 'h',     mods = 'ALT|WIN|CTRL',  action = act.AdjustPaneSize { 'Left', 5 } },
+    { key = 'l',     mods = 'ALT|WIN|CTRL',  action = act.AdjustPaneSize { 'Right', 5 } },
+    { key = 'j',     mods = 'ALT|WIN|CTRL',  action = act.AdjustPaneSize { 'Down', 5 } },
+    { key = 'k',     mods = 'ALT|WIN|CTRL',  action = act.AdjustPaneSize { 'Up', 5 } },
+    { key = 'z',     mods = 'ALT|WIN',       action = act.TogglePaneZoomState },
+    { key = 's',     mods = 'ALT|WIN',       action = act.ReloadConfiguration },
+    { key = '=',     mods = 'ALT|WIN',       action = act.IncreaseFontSize },
+    { key = '-',     mods = 'ALT|WIN',       action = act.DecreaseFontSize },
+    { key = '0',     mods = 'ALT|WIN',       action = act.ResetFontSize },
+    { key = 'y',     mods = 'ALT|WIN',       action = act.CopyTo 'Clipboard' },
+    { key = 'p',     mods = 'ALT|WIN',       action = act.PasteFrom 'Clipboard' },
+    -- { key = 'N', mods = 'SHIFT|CTRL', action = act.SpawnWindow },
+    { key = ',',     mods = 'ALT|WIN',       action = act.ActivateTabRelative(-1) },
+    { key = '.',     mods = 'ALT|WIN',       action = act.ActivateTabRelative(1) },
+    { key = 'v',     mods = 'ALT|WIN',       action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = 't',     mods = 'ALT|WIN',       action = act.SpawnTab 'CurrentPaneDomain' },
+    { key = 'w',     mods = 'ALT|WIN',       action = act.CloseCurrentTab { confirm = false } },
+    { key = 'x',     mods = 'ALT|WIN',       action = act.CloseCurrentPane { confirm = false } },
+    -- { key = 'b', mods = 'LEADER|CTRL', action = act.SendString '\x02', },
+    { key = 'Enter', mods = 'ALT|WIN',       action = act.ActivateCopyMode, },
+    --[[ { key = 'k', mods = 'CTRL|ALT', action = act.Multiple
+      {
+        act.ClearScrollback 'ScrollbackAndViewport'
+        act.SendKey { key = 'L', mods = 'CTRL' }
+      }
+    } ]]
+    -- { key = 'r', mods = 'LEADER', action = act.ActivateKeyTable { name = 'resize_pane', one_shot = false, } },
+  },
+  wsl_domains = wsl_domains,
+}
