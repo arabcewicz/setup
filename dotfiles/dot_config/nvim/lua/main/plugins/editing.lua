be7217ceba8f8b34fd8c25d9acd8a3805cc4a6fe
Adam Rabcewicz <arabcewicz@evolution.com>
2025-02-04 08:28:21 +0100

    nvim: grug-far

--- dotfiles/dot_config/nvim/lua/main/plugins/editing.lua
+++ dotfiles/dot_config/nvim/lua/main/plugins/editing.lua
@@ -156,12 +156,35 @@ return {
     opts = {},
     -- stylua: ignore
     keys = {
-      { "]t",         function() require("todo-comments").jump_next() end,              desc = "Next Todo Comment" },
-      { "[t",         function() require("todo-comments").jump_prev() end,              desc = "Previous Todo Comment" },
-      { "<leader>xt", "<cmd>Trouble todo toggle<cr>",                                   desc = "Todo (Trouble)" },
-      { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-      { "<leader>st", "<cmd>TodoTelescope<cr>",                                         desc = "Todo" },
-      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>",                 desc = "Todo/Fix/Fixme" },
+      { "]t",         function() require("todo-comments").jump_next() end,              desc = "todo-comments: Next Todo Comment" },
+      { "[t",         function() require("todo-comments").jump_prev() end,              desc = "todo-comments: Previous Todo Comment" },
+      { "<leader>xt", "<cmd>Trouble todo toggle<cr>",                                   desc = "todo-comments: Todo (Trouble)" },
+      { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "todo-comments: Todo/Fix/Fixme (Trouble)" },
+      { "<leader>st", "<cmd>TodoTelescope<cr>",                                         desc = "todo-comments: Todo" },
+      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>",                 desc = "todo-comments: Todo/Fix/Fixme" },
+    },
+  },
+
+  {
+    "MagicDuck/grug-far.nvim",
+    opts = { headerMaxWidth = 80 },
+    cmd = "GrugFar",
+    keys = {
+      {
+        "<leader>sr",
+        function()
+          local grug = require("grug-far")
+          local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+          grug.open({
+            transient = true,
+            prefills = {
+              filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+            },
+          })
+        end,
+        mode = { "n", "v" },
+        desc = "grug-far: Search and Replace",
+      },
     },
   },
 }
