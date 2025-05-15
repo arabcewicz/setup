bbb58dab907ae6b7001f893e13f777a253bbd7c1
Adam Rabcewicz <arabcewicz@evolution.com>
2025-05-15 18:25:47 +0200

    yazi: git support

--- dotfiles/dot_config/yazi/init.lua
+++ dotfiles/dot_config/yazi/init.lua
@@ -1,4 +1,7 @@
 require("folder-rules"):setup()
 --
 --ya pack -a yazi-rs/plugins:full-border
-require("full-border"):setup()
+-- require("full-border"):setup()
+
+require("git"):setup()
+require("starship"):setup()
